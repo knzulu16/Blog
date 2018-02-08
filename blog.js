d023fb11769e9d5da835dafa293550ce561a2120
Nonzulu <knzulu16@gmail.com>
2018-02-08 15:24:44 +0200

    pushing to master

---
+++ blog.js
@@ -0,0 +1,43 @@
+module.exports = function(blogWriter) {
+  const post = function(req, res){
+    var storeModel = blogWriter.BlogPost;
+    var name=req.body.name;
+    var date=req.body.date;
+    var comment=req.body.comment;
+     // console.log("m found",req.body);
+        storeModel.create({
+          name:name,
+          date:date,
+          comment:comment
+        },function(err,store){
+          if(err){
+            console.log(err);
+          }
+          else{
+            res.json({results:store})
+          }
+
+        })
+        }
+
+
+const getAllComments=function(req,res){
+  var storeModel = blogWriter.BlogPost;
+storeModel.find({},function(err,retriev){
+  if(err){
+    console.log(err);
+  }
+  else{
+    res.json({
+      retriev
+    })
+  }
+})
+
+}
+
+  return {
+    post,
+    getAllComments
+  }
+}
